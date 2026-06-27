4a588661fc1d68f7b14efe40502bd30b5dcad7f1
Daniel Veis Yaganeh <danve143@student.liu.se>
2026-06-27 23:22:54 +0200

    Bug fix, encoding in utf-8

--- src/character.py
+++ src/character.py
@@ -1,5 +1,5 @@
 def getCharacterName(filename):
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()
     
     if not lines:
@@ -11,10 +11,10 @@ def getCharacterName(filename):
     return character
 
 def removeCharacterName(filename, character):
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()
     
-    with open(filename, 'w') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         for line in lines:
             if line.strip() != character:
                 f.write(line)
